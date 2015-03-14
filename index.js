951a126556f24ed0ec0f42620e0a339d633f8306
Kirk Elliott <kirk@addthis.com>
2015-03-13 22:30:30 -0400

    changes for heroku

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.set('views', __dirname + '/src');
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
